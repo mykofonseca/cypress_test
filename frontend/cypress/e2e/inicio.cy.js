a9802aae6bd332a607c948e3d034d25461aedd1a
unknown <mykosistema@gmail.com>
2024-08-16 15:09:40 -0300

    Alteração exercício 5

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -2,7 +2,7 @@ describe('Página Principal', () => {
   beforeEach( () => {
     cy.visit('http://localhost:3000/')
   })
-  it('Deve renderizar h1 com o texto!', () => {
+  it('Renderização de todas as seções e itens da página inicial', () => {
     cy.visit('http://localhost:3000')
     cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!');
     cy.getByData('sub-titulo').contains('Minha nossa, nossa, nossa!');
