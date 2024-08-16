67857bee14f993e1e49e485e1b0cca9a41cedd87
unknown <mykosistema@gmail.com>
2024-08-16 08:50:12 -0300

    Salvamento de conteúdo pós exercício 2, atividade 5

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -1,6 +1,9 @@
 describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
   it('Deve renderizar h1 com o texto!', () => {
     cy.visit('http://localhost:3000')
-    cy.get('h2').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
 })
--- frontend/src/paginas/Inicio/index.jsx
+++ frontend/src/paginas/Inicio/index.jsx
@@ -15,7 +15,7 @@ export default function Inicio() {
             <h1 data-test="titulo-principal">
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
-            </h1>
+            </h1 >
             <img
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
