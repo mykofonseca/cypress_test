6cc3980a78db66dc5d911e15fb81d51151a4fb68
unknown <mykosistema@gmail.com>
2024-08-22 09:46:08 -0300

    Correção dos erros na atividade 7, 8 e 9, correção na depuração das funções de teste

--- frontend/cypress/e2e/formularioCadastro.cy.js
+++ frontend/cypress/e2e/formularioCadastro.cy.js
@@ -5,11 +5,11 @@ describe('Formulário cadastro', () => {
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() /* testaremos agora o botão 'cadastrar' */
     cy.getByData('nome-input').type('Jose da Silva')
-    cy.getByData('email-input').type('a@email.com')
+    cy.getByData('email-input').type('myko@email.com')
     cy.getByData('senha-input').type('456789')
     cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-sucesso').should('exist')
-    //.and('have-text', 'Usuário cadastrado com sucesso!')
+    cy.getByData('nome-input').should('exist')
+    cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
     
     //o campo acima não é capaz de realizar a detecção do campo 'have-text', pois o campo não se encontra disponível, assim como no exercício anterior. Revisão necessária .
 
--- frontend/cypress/e2e/formularioLogin.cy.js
+++ frontend/cypress/e2e/formularioLogin.cy.js
@@ -1,11 +1,23 @@
-it.only('Não deve permitir um campo em branco', () => {
-    cy.visit('/')
-    cy.getByData('botao-login').click()
-    // cy.getByData('email-input').type('neilton@alura.com')
-    cy.getByData('senha-input').should('exist')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist')
-    // .and('have-text', "o campo email é obrigatório")
-
-    //o campo acima não é capaz de realizar a detecção do campo 'have-text', pois o campo não se encontra disponível. Revisão necessária .
+describe('Formulário de login', () => { // contexto do nosso teste
+    beforeEach(()=> {
+    cy.visit('http://localhost:3000')
     })
+
+    it('Não deve permitir um e-mail inválido!', () => {
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+    
+    it.only('Não deve permitir um campo em branco', () => {
+        cy.getByData('botao-login').click()
+        // cy.getByData('email-input').type('')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
+        })
+
+    })
+    })
+
