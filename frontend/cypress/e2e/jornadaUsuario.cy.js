24f1ac9d1beeb6ee1115bc025d3091a5db3cdda1
unknown <mykosistema@gmail.com>
2024-08-23 13:48:53 -0300

    Salvamento pós correção atividade 12, adição da função depósito

--- frontend/cypress/e2e/jornadaUsuario.cy.js
+++ frontend/cypress/e2e/jornadaUsuario.cy.js
@@ -1,4 +1,4 @@
-describe('Jornadas de usuário', () => {
+describe('Jornadas de usuário (Transferência)', () => {
     it('Deve permitir que o usuário acesse a aplicação, realize uma transação e faça logout', () => {
     cy.visit('/')
     //login
@@ -10,11 +10,32 @@ describe('Jornadas de usuário', () => {
     cy.location('pathname').should('eq','/home')
     //transferencia e valor
     cy.getByData('select-opcoes').select('Transferência') 
-    cy.getByData('form-input').type('80')
+    cy.getByData('form-input').type('1000')
     cy.getByData('realiza-transacao').click()
-    cy.getByData('lista-transacoes').find('li').last().contains('- R$ 80')
+    cy.getByData('lista-transacoes').find('li').last().contains('- R$ 1000')
     //logout
     cy.getByData('botao-sair').click()
     cy.location('pathname').should('eq', '/')
+    })
+})
+
+describe('Jornadas de usuário (Depósito)', () => {
+    it('Deve permitir que o usuário acesse a aplicação, realize uma transação e faça logout', () => {
+    cy.visit('/')
+    //login
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    //verificação página inicial
+    cy.location('pathname').should('eq','/home')
+    //transferencia e valor
+    cy.getByData('select-opcoes').select('Depósito') 
+    cy.getByData('form-input').type('1000')
+    cy.getByData('realiza-transacao').click()
+    cy.getByData('lista-transacoes').find('li').last().contains('R$ 1000')
+    //logout
+    cy.getByData('botao-sair').click()
+    cy.location('pathname').should('eq', '/')   
     })
 })
